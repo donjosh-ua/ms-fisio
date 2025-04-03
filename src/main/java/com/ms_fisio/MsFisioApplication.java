ab1f53d2e0319adedc1e61575b17929305c9c498
JOSHUA EFRAIN CUEVA CAMPOVERDE joshua.cueva@ucuenca.edu.ec <joshua.cueva@ucuenca.edu.ec>
2025-04-03 09:58:05 -0500

    JC-Init codebase

---
+++ src/main/java/com/ms_fisio/MsFisioApplication.java
@@ -0,0 +1,13 @@
+package com.ms_fisio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFisioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFisioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms_fisio/MsFisioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_fisio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFisioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
